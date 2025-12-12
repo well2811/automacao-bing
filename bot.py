94037d6054c5a1a0fee11f26c4379891b2713c73
well2811 <wellingtonx8@gmail.com>
2025-12-12 12:01:25 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.bing.com")
+
+print("Título da página:", driver.title)
+
+driver.quit()
